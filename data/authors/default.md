c429b7c6f55cfdb674069d028e45a91faad249d9
lzs <2468979982@qq.com>
2025-06-20 20:15:56 +0800

    init

--- data/authors/default.md
+++
@@ -1,6 +0,0 @@
----
-name: lzs39
-avatar: ../static/images/avatar.png
-occupation: 初级程序员
----
-一位边学习边成长的软件工程师。
--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  // { href: '/columns', title: 'Column' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
